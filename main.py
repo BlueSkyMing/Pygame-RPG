3b8b413b8fab500374e6f5b004091f50b9208792
BlueSkyMing <luximing0141@outlook.com>
2024-02-03 12:41:32 +0800

    打了点注释

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #coding=utf-8
 import pygame
 import sys
-#初始化
+#初始化 - Init
 pygame.init()
 screen=pygame.display.set_mode((800,600))
 clock=pygame.time.Clock()
-
+#主程序 - Main
 while True:
     clock.tick(60)
     for event in pygame.event.get():
